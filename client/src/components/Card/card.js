66a6c813ba73efdaf2978beda998983ec3da2851
jothamardel <ardelmbiplang@gmail.com>
2022-07-28 02:58:50 +0100

    chore: updating application

--- client/src/components/Card/card.js
+++ client/src/components/Card/card.js
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
 import "tachyons";
 import "./card.css";
-const Card = () => {
+const Card = ({ fullName, email, photoUrl}) => {
   const [click, setClick] = useState("ABSCENT");
   const handleClick = () => setClick("PRESENT");
 
   return (
     <div>
-      <div className="bg-light-green dib br3 pa3 ma2 bw2 shadow-5">
-        <img alt="robots" src="https://robohash.org/hello?200*200" />
-        <div>
-          <h2>john</h2>
-          <p>john@gmail.com</p>
+      <div className="bg-transparent dib br3 pa3 ma2 bw2 shadow-5">
+        <img alt="robots" src={!photoUrl ? `https://robohash.org/${fullName}?200*200` : photoUrl}/>
+        <div style={{ textAlign: 'center'}}>
+          <h2>{fullName}</h2>
+          <p>{ email }</p>
         </div>
-        <div className="toggle">
-          <button onClick={handleClick}>{click}</button>
+        <div style={{textAlign: 'center'}}>
+          <button onClick={handleClick} style={{ fontSize: '1rem', padding: '0.7rem 2rem'}}>{click}</button>
         </div>
       </div>
     </div>
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,6 +3,32 @@ import Card from "../../components/Card/card";
 import background from "../../assets/background.jpg";
 import Navigation from "../../components/Navigation/Navigation";
 import "./home.css";
+
+
+const data = [
+  {
+    fullName: 'John Doe',
+    email: 'john@email.com',
+    photoUrl: ''
+  },
+  {
+    fullName: 'Sarah Doe',
+    email: 'sarah@email.com',
+    photoUrl: ''
+  },
+  {
+    fullName: 'Peter Doe',
+    email: 'peter@email.com',
+    photoUrl: ''
+  },
+  {
+    fullName: 'Kate Doe',
+    email: 'kate@email.com',
+    photoUrl: ''
+  },
+]
+
+
 const Home = () => {
   return (
     <section>
@@ -17,9 +43,16 @@ const Home = () => {
           padding: "1rem",
         }}
       >
-        <h1>Browse Our Students Catelog</h1>
+        <h1>ITF/NECA Student attendance system</h1>
       </div>
-      <Card />
+      <div style={{ padding: '2rem', display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+        {
+          data.map((item, index) => (
+            <Card key={index.toString()} {...item}/>
+          ))
+        }
+ 
+      </div>
     </section>
   );
 };
